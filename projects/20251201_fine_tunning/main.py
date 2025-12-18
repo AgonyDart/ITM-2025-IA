3c8664f603b9e109f368a470191e9c5e0345b1f2
Angel Duarte <adavid.tinocod@gmail.com>
2025-12-18 11:55:44 -0600

    [ADD] fine tunv1

--- projects/20251201_fine_tunning/main.py
+++ projects/20251201_fine_tunning/main.py
@@ -5,10 +5,10 @@ from transformers import TrainingArguments
 from datasets import load_dataset
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="unsloth/llama-3-8b-bnb-4bit",
-    max_seq_length=512,
+    model_name="unsloth/llama-3.2-3b-bnb-4bit",
+    max_seq_length=1024,
     load_in_4bit=True,
-    device_map="sequential",
+    # device_map="sequential",
 )
 
 torch.cuda.empty_cache()
@@ -27,19 +27,25 @@ dataset = load_dataset("json", data_files="dataset.jsonl", split="train")
 
 trainer = SFTTrainer(
     model=model,
+    tokenizer=tokenizer,
     train_dataset=dataset,
     dataset_text_field="text",
     max_seq_length=1024,
     args=TrainingArguments(
-        per_device_train_batch_size=2,
-        gradient_accumulation_steps=4,
-        max_steps=100,
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=8,
+        warmup_steps=5,
+        max_steps=50,
         learning_rate=2e-4,
         fp16=True,
         logging_steps=1,
         output_dir="outputs",
+        optim="adamw_8bit",
+        report_to="none",
+        push_to_hub=False,
     ),
 )
+
 trainer.train()
 
 model.save_pretrained_gguf("model_tutor", tokenizer, quantization_method="q4_k_m")
